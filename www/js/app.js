5bb70869b8502feb76f7690f448dcffe79ce2ab7
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2018-06-08 19:26:38 +0200

    index was modified with infinite scroll app was modified to get new pics, and to know when customer scroll down the API send us new posts to show.
    This function is to stop scroll and show us new 25 post
          $scope.$broadcast('scroll.infiniteScrollComplete');

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,7 @@
 (function() {
 
     var app = angular.module('starter', ['ionic', 'angularMoment']);
-    var urlRedditAPI = 'https://www.reddit.com/r/pics/.json';
+    var urlRedditAPI = 'https://www.reddit.com/r/pics/new/.json';
 
     app.controller('RedditController', function($scope, $http) {
         $scope.posts = [];
@@ -13,6 +13,27 @@
                     console.log(post);
                 });
             });
+
+        $scope.loadMorePost = function() {
+
+            var paramsFromURL = {};
+
+            if ($scope.posts.lenght > 0) {
+                paramsFromURL['after'] = $scope.posts[$scope.posts.lenght - 1].name;
+            }
+
+            $http.get(urlRedditAPI, { params: paramsFromURL })
+                .success(function(posts) {
+                    // console.log(posts);
+                    angular.forEach(posts.data.children, function(post) {
+                        $scope.posts.push(post.data);
+                        console.log(post);
+                    });
+
+                    $scope.$broadcast('scroll.infiniteScrollComplete');
+
+                });
+        };
     });
 
     app.run(function($ionicPlatform) {
