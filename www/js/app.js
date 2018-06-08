59d0fb7760bfd7d9a78fbcbac6f37eee47bcae8a
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2018-06-08 13:28:47 +0200

    app.controller was added to app.js with dummies data index was modified to delete commented lines and to implement RedditController and to show posts dummy

--- www/js/app.js
+++ www/js/app.js
@@ -2,6 +2,13 @@
 
     var app = angular.module('starter', ['ionic']);
 
+    app.controller('RedditController', function($scope) {
+        $scope.posts = [
+            { title: 'Primer post' },
+            { title: 'Segundo post' }
+        ];
+    });
+
     app.run(function($ionicPlatform) {
         $ionicPlatform.ready(function() {
             if (window.cordova && window.cordova.plugins.Keyboard) {
